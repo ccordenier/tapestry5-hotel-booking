1aef661a55605143dc8e587370dce5b8632c3e94
ccordenier <christophe.cordenier@gmail.com>
2010-09-20 21:59:36 +0200

    Initialize project repository

---
+++ src/main/java/com/tap5/hotelbooking/pages/Index.java
@@ -0,0 +1,14 @@
+package com.tap5.hotelbooking.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application tapestry5-hotel-booking.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
