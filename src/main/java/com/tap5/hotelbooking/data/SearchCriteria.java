2c130cf61a2f5f4720440a59eca1801f51612429
ccordenier <christophe.cordenier@gmail.com>
2010-09-29 07:45:39 +0200

    Improve Search page logic, still need to stylize

---
+++ src/main/java/com/tap5/hotelbooking/data/SearchCriteria.java
@@ -0,0 +1,62 @@
+package com.tap5.hotelbooking.data;
+
+import java.io.Serializable;
+
+/**
+ * This class has been extracted from JBoss seam example applications
+ */
+public class SearchCriteria implements Serializable
+{
+    private static final long serialVersionUID = 6022601867962992033L;
+
+    private static final char SQL_WILDCARD_CHAR = '%';
+
+    private static final String SQL_WILDCARD_STR = String.valueOf(SQL_WILDCARD_CHAR);
+
+    private static final String REPEAT_SQL_WIDCARD_REGEX = SQL_WILDCARD_STR + "+";
+
+    private static final char HUMAN_WILDCARD_CHAR = '*';
+
+    private String query = "";
+
+    private int rowsPerPage = 10;
+    
+    public String getSearchPattern()
+    {
+        if (query == null || query.length() == 0) { return SQL_WILDCARD_STR; }
+
+        StringBuilder pattern = new StringBuilder();
+        pattern.append(query.toLowerCase().replace(HUMAN_WILDCARD_CHAR, SQL_WILDCARD_CHAR)
+                .replaceAll(REPEAT_SQL_WIDCARD_REGEX, SQL_WILDCARD_STR));
+        if (pattern.length() == 0 || pattern.charAt(0) != SQL_WILDCARD_CHAR)
+        {
+            pattern.insert(0, SQL_WILDCARD_CHAR);
+        }
+        if (pattern.length() > 1 && pattern.charAt(pattern.length() - 1) != SQL_WILDCARD_CHAR)
+        {
+            pattern.append(SQL_WILDCARD_CHAR);
+        }
+        return pattern.toString();
+    }
+
+    public String getQuery()
+    {
+        return query;
+    }
+
+    public void setQuery(String query)
+    {
+        this.query = (query != null ? query.trim() : null);
+    }
+
+    public int getRowsPerPage()
+    {
+        return rowsPerPage;
+    }
+
+    public void setRowsPerPage(int rowsPerPage)
+    {
+        this.rowsPerPage = rowsPerPage;
+    }
+
+}
--- src/main/java/com/tap5/hotelbooking/pages/Search.java
+++ src/main/java/com/tap5/hotelbooking/pages/Search.java
@@ -4,6 +4,7 @@ import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.hibernate.HibernateGridDataSource;
@@ -12,6 +13,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
+import com.tap5.hotelbooking.data.SearchCriteria;
 import com.tap5.hotelbooking.domain.entities.Hotel;
 
 /**
@@ -21,6 +23,20 @@ import com.tap5.hotelbooking.domain.entities.Hotel;
  */
 public class Search
 {
+
+    @Inject
+    private Session session;
+
+    @InjectComponent
+    private Zone result;
+
+    @SessionState
+    @Property
+    private SearchCriteria criteria;
+
+    @Property
+    private GridDataSource source;
+
     /**
      * This datasource is used by Tapestry 5 Grid to search and paginate
      * 
@@ -36,31 +52,14 @@ public class Search
         @Override
         protected void applyAdditionalConstraints(Criteria crit)
         {
-            crit.add(Restrictions.like("name", query));
+            crit.add(Restrictions.like("name", criteria.getSearchPattern()));
         }
     }
 
-    @Inject
-    private Session session;
-
-    @InjectComponent
-    private Zone result;
-
-    @Property
-    private String query;
-
-    @Property
-    private int rowsPerPage;
-
-    @Property
-    private GridDataSource source;
-
     @OnEvent(value = EventConstants.ACTIVATE)
-    void onActivate(String query, int rowsPerPage)
+    void prepareDataSource()
     {
-        this.query = query;
-        this.rowsPerPage = rowsPerPage;
-        this.source = new HotelDataSource(session, Hotel.class);
+        source = new HotelDataSource(session, Hotel.class);
     }
 
     /**
@@ -69,15 +68,8 @@ public class Search
     @OnEvent(value = EventConstants.SUCCESS)
     Object searchHotels()
     {
-        source = new HotelDataSource(session, Hotel.class);
+        prepareDataSource();
         return result;
-    }
-
-    @OnEvent(value = EventConstants.PASSIVATE)
-    Object[] onPassivate()
-    {
-        return new Object[]
-        { this.query, this.rowsPerPage };
     }
 
 }
