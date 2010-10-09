ff382781754402b721162e87c22fb39c0df8ebbd
Katia Aresti <karesti@MacBook-Pro-de-Katia-Aresti.local>
2010-10-09 11:45:17 +0200

    RequiresLogin corrected
    Annotation corrected in order to be detected by the security filter

--- src/main/java/com/tap5/hotelbooking/annotations/RequiresLogin.java
+++ src/main/java/com/tap5/hotelbooking/annotations/RequiresLogin.java
@@ -1,5 +1,11 @@
 package com.tap5.hotelbooking.annotations;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Security annotation for Tapestry Pages. Prevents anonymous access to pages. The current user must
  * be logged in.
@@ -7,6 +13,10 @@ package com.tap5.hotelbooking.annotations;
  * @author karesti
  * @version 1.0
  */
+@Target(
+{ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface RequiresLogin
 {
 
