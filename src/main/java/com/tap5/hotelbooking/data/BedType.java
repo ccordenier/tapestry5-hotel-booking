30d0410b4d2d582dc2095ee46c6bdc89349eb06d
ccordenier <christophe.cordenier@gmail.com>
2010-10-11 13:19:44 +0200

    Customize booking form

---
+++ src/main/java/com/tap5/hotelbooking/data/BedType.java
@@ -0,0 +1,40 @@
+package com.tap5.hotelbooking.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tapestry5.OptionGroupModel;
+import org.apache.tapestry5.OptionModel;
+import org.apache.tapestry5.internal.OptionModelImpl;
+import org.apache.tapestry5.util.AbstractSelectModel;
+
+/**
+ * Represent bed types supported by the application. 
+ *
+ * @author ccordenier
+ *
+ */
+public class BedType extends AbstractSelectModel
+{
+
+    private List<OptionModel> options = new ArrayList<OptionModel>();
+
+    public BedType()
+    {
+        options.add(new OptionModelImpl("One king-sized bed", 1));
+        options.add(new OptionModelImpl("Two double beds", 2));
+        options.add(new OptionModelImpl("Three beds", 3));
+    }
+
+    public List<OptionGroupModel> getOptionGroups()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<OptionModel> getOptions()
+    {
+        return options;
+    }
+
+}
---
+++ src/main/java/com/tap5/hotelbooking/data/Years.java
@@ -0,0 +1,39 @@
+package com.tap5.hotelbooking.data;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.apache.tapestry5.OptionGroupModel;
+import org.apache.tapestry5.OptionModel;
+import org.apache.tapestry5.internal.OptionModelImpl;
+import org.apache.tapestry5.util.AbstractSelectModel;
+
+public class Years extends AbstractSelectModel
+{
+
+    private List<OptionModel> options = new ArrayList<OptionModel>();
+
+    public Years()
+    {
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = 0; i < 6; i++)
+        {
+            options.add(new OptionModelImpl(year + i + ""));
+        }
+
+    }
+
+    public List<OptionGroupModel> getOptionGroups()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<OptionModel> getOptions()
+    {
+        return options;
+    }
+
+}
